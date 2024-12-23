73e975a07121a694f79a0be91be060ce9592566c
JohannesTewinkel <jojo.tewinkel@gmail.com>
2024-12-23 17:28:45 +0100

    clemens

--- src/slices/Section/index.js
+++ src/slices/Section/index.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { PrismicRichText } from "@/components/PrismicRichText";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
@@ -7,42 +7,40 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
  * Component for "Section" Slices.
  */
 const Section = ({ slice }) => {
-  // Determine the text color class
-  const textColorClass = slice.primary.whitetext ? 'text-white' : 'text-black';
-
   return (
     <section
-      data-slice-type={slice.slice_type}  
+      data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className="section"
     >
       <div className="backdrop">
         <PrismicNextImage field={slice.primary.backdrop} />
       </div>
-    
+
       {slice.primary.content.map((item, index) => (
-        <div key={index} className={`flex ${slice.primary.alignleft ? 'left' : ''}`}>
+        <div
+          key={index}
+          className={`flex ${slice.primary.alignleft ? 'left' : ''} ${
+            slice.primary.whitetext ? 'text-white' : 'text-black'
+          }`}
+        >
           <div className="bild">
             <PrismicNextImage field={item.image} />
-            <model-viewer  
-              src={item.model3d.url} 
-              ar 
+            <model-viewer
+              src={item.model3d.url}
+              ar
               shadow-intensity="1"
-              camera-controls 
+              camera-controls
               touch-action="pan-y"
             ></model-viewer>
           </div>
-          
-          <div className={`flistext ${textColorClass}`}>
-            <div className="title">
-              {item.title}
-            </div>
+
+          <div className="flistext">
+            <div className="title">{item.title}</div>
             <div className="text">
               <PrismicRichText field={item.text} />
             </div>
-            <div className="button">
-              {item.button}
-            </div>
+            <div className="button">{item.button}</div>
           </div>
         </div>
       ))}
